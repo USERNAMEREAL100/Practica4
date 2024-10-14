b33a804effa75e4538bf0084025ea36a1db9b9d3
USERNAMEREAL100 <127427691+USERNAMEREAL100@users.noreply.github.com>
2024-10-14 07:31:58 +0200

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть число N: ");
+        int N = scanner.nextInt();
+
+        System.out.println("Прості числа від 1 до " + N + ":");
+        for (int i = 2; i <= N; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+
+        scanner.close();
+    }
+}
